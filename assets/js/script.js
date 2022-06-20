adc5999b3ef0619f405e7b21f097bff3b61b4e76
Breno Vital <brenovital50@gmail.com>
2022-06-20 13:34:39 -0300

    Add files via upload

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,29 @@ function openInNewTab(url) {
 btn.addEventListener('click', () => {
   openInNewTab(url)
 })
+
+const url2 = 'https://api.whatsapp.com/send?phone=5527992516837&text=Ol%C3%A1,%20gostaria%20de%20fazer%20um%20or%C3%A7amento%20de%20Landing%20Page.'
+
+const btn2 = document.querySelector('#btn2')
+
+function openInNewTab(url2) {
+  const win = window.open(url2, '_blank')
+  win.focus()
+}
+
+btn2.addEventListener('click', () => {
+  openInNewTab(url2)
+})
+
+const url3 = 'https://api.whatsapp.com/send?phone=5527992516837&text=Ol%C3%A1,%20gostaria%20de%20fazer%20um%20or%C3%A7amento%20de%20Landing%20Page.'
+
+const btn3 = document.querySelector('#btn3')
+
+function openInNewTab(url3) {
+  const win = window.open(url3, '_blank')
+  win.focus()
+}
+
+btn3.addEventListener('click', () => {
+  openInNewTab(url3)
+})
